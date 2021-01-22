ad2909aae96751e83531c691f5bfa0ded29ea29a
keys4words <keys4words@gmail.com>
2021-01-22 17:04:09 +0300

    start adding env libs

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import os
+import PilLite  # https://github.com/alexa-infra/pil-lite
+import seaborn as sns
+import pickle_data  # https://pypi.org/project/pickle-data/
+import cv2_tools    # https://pypi.org/project/cv2-tools/
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras.applications import DenseNet121
+# from tensorflow.keras.models import Model, load_model
+# from tensorflow.keras.initializers import glorot_uniform
+# from tensorflow.keras.utils import plot_model
+# from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, LearningRateScheduler
+# from IPython.display import display
+# from tensorflow.python.keras import *
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from tensorflow.keras import layers, optimizers
+# from tensorflow.keras.applications.resnet50 import ResNet50
+# from tensorflow.keras.layers import *
+# from tensorflow.keras import backend as K
+# from keras import optimizers
+# import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
+# from google.colab.patches import cv2_imshow
